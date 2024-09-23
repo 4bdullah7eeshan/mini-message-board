2b5ae2cf7659b0025a0f40677f12221806ed5001
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 21:32:06 +0530

    feat: place sample messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,20 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+];
+  
+
 indexRouter.get("/", (req, res) => {
     res.send("Mini Message Board!");
 });
