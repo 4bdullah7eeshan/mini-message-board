248957d912d137103ae1136a1ffc67841ae24730
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-24 01:54:50 +0530

    feat(style): add styling
    NOTE: Never do './resets.css' but '/resets.css' when linking from an ejs file.

--- routes/openMessageRouter.js
+++ routes/openMessageRouter.js
@@ -4,10 +4,15 @@ const messages = require("../data/messages");
 const openMessageRouter = Router();
 
 openMessageRouter.get("/:id", (req, res) => {
+    console.log("Request URL:", req.originalUrl); // Log the full request URL
+
     const messageId = parseInt(req.params.id, 10); 
+    console.log("Requested Message ID:", messageId); // Add this line
+
     const message = messages.find(msg => msg.id === messageId);
 
-    res.render("pages/message", { title: message.text, message });    
+    res.render("pages/message", { title: message.text, message: message });
+
 });
 
 module.exports = openMessageRouter;
