97a12b485054d84ee44d92a29f936604fda0cbb2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 21:57:16 +0530

    feat: setup a new message form

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const newMessageRouter = Router();
 
 newMessageRouter.get("/", (req, res) => {
-    res.send("New Message");
+    res.render("partials/form");
 });
 
 module.exports = newMessageRouter;
