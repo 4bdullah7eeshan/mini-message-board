336f9c1b1a5cd5bd0a6d8283c15998c6cc672219
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-24 23:16:34 +0530

    wip: setup controllers
    I will be refactoring the code to use controllers. This is not finalized yet, because I will be adding a db.

---
+++ controllers/messageController.js
@@ -0,0 +1,18 @@
+const asyncHandler = require("express-async-handler");
+
+const getAllMessages = asyncHandler(async (req, res) => {
+
+});
+
+const createNewMessage = asyncHandler(async (req, res) => {
+
+});
+
+const getNewMessage = asyncHandler(async (req, res) => {
+
+});
+
+const getMessageById = asyncHandler(async (req, res) => {
+
+});
+
