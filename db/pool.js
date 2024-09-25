3dc72ddb3b840672999769b9dbe6e139835f615c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-25 18:36:21 +0530

    fix: use require sslmode

--- db/pool.js
+++ db/pool.js
@@ -6,5 +6,8 @@ module.exports = new Pool({
     user: process.env.PGUSER,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT || 5432
+    port: process.env.PGPORT || 5432,
+    ssl: {
+        require: true,
+    }
 });
